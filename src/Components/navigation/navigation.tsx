d162f164cc1b17c7b9a49b1262b2fb04cd920582
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-05-07 23:08:13 +0700

    Responsive config

--- src/Components/navigation/navigation.tsx
+++ src/Components/navigation/navigation.tsx
@@ -15,15 +15,15 @@ type Props = {}
 export default function Navigation({}: Props) {
     return (
         <section className='w-full sticky top-0 z-10'>
-            <div className='flex flex-row w-full items-center justify-between mobile:px-[3vw] text-gray-800 font-medium mobile:justify-between md:text-lg lg:text-xl lg:px-[3vw] xl:px-[6vw] '>
-                <span className='mobile:hidden md:flex hover:bg-gray-100 h-full py-[2vh] px-[2vw]'>
+            <div className='flex flex-row w-full items-center justify-between mobile:px-[3vw] sm:px-0 text-gray-800 font-medium mobile:justify-between md:text-lg lg:text-xl lg:px-[3vw] xl:px-[6vw] '>
+                <span className='mobile:hidden sm:flex hover:bg-gray-100 h-full py-[2vh] px-[2vw]'>
                     <Link href="/">
-                        <RiHotelLine className='text-3xl  font-thin'/>
+                        <RiHotelLine className='text-2xl  font-thin'/>
                     </Link>
                 </span>
 
-                <ul className='flex flex-row gap-x-[2vw] lg:gap-x-[3vw] items-center sm:w-full'>
-                    <ul className='sm:flex md:hidden flex-row items-center w-full justify-between'>
+                <ul className='flex flex-row gap-x-[2vw] lg:gap-x-[3vw] items-center mobile:w-full h-full'>
+                    <ul className='mobile:flex sm:hidden flex-row items-center w-full justify-between'>
                         {/* responsive handle < sm */}
                         <li className='justify-center items-center hover:bg-gray-100 h-full py-[2vh] px-[2vw]'>
                             <IoMenuOutline className='text-2xl'/>                            
@@ -35,7 +35,7 @@ export default function Navigation({}: Props) {
                         </li>
                     </ul>
 
-                    <ul className='mobile:hidden md:flex flex-row items-center  w-full transition-all'>
+                    <ul className='mobile:hidden sm:flex flex-row items-center  w-full transition-all h-full'>
                         <li className='flex w-full justify-center items-center hover:bg-gray-100  h-full py-[2vh] px-[2vw]'>
                             <Link href='/about-us'>
                                 <h1 className='lg:text-xl md:text-base'>ABOUT US</h1>
@@ -45,18 +45,17 @@ export default function Navigation({}: Props) {
                             <Link href='/room'>
                                 <h1 className='lg:text-xl md:text-base'>ROOM</h1>
                             </Link>
-                        </li>      
+                        </li>
                         <li className='flex w-full justify-center items-center hover:bg-gray-100 h-full py-[2vh] px-[2vw]'> 
                             <Link href='/facilities'>
                                 <h1 className='lg:text-xl md:text-base'>FACILITIES</h1>
                             </Link>
-                        </li>        
+                        </li> 
                         <li className='flex w-full justify-center items-center hover:bg-gray-100 h-full py-[2vh] px-[2vw]'> 
-                            <Link href='/offers'>
-                                <h1 className='lg:text-xl md:text-base'>OFFERS</h1>
+                            <Link href='/location'>
+                                <h1 className='lg:text-xl md:text-base'>LOCATIONS</h1>
                             </Link>
-                        </li>  
-
+                        </li>
                         <li className='flex w-full justify-center items-center hover:bg-gray-100 h-full py-[2vh] px-[2vw]'> 
                             <Link href='/contact'>
                                 <h1 className='lg:text-xl md:text-base'>CONTACT</h1>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Navigation from "@/Components/navigation/navigation";
 export default function Home() {
   return (
     <main className="">
-            
+        
     </main>
   );
 }
