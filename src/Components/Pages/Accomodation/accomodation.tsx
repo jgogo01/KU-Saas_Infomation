db930e2e85d5d029942b5f6827cba28a212bb41a
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-05-09 00:42:53 +0700

    Detailing each accommodation types

--- src/Components/Pages/Accomodation/accomodation.tsx
+++ src/Components/Pages/Accomodation/accomodation.tsx
@@ -48,14 +48,13 @@ export default function Accomodation({}: Props) {
             </li>
 
            <li className='bg-gray-50'>
-               <div className='flex flex-row items-center justify-center w-full hover:p-10 transition-all duration-1000'> 
+              <div className='flex flex-row items-center justify-center w-full hover:p-10 transition-all duration-1000'> 
                 <div className='flex flex-col w-full text-center gap-y-[1vh] mobile:py-[3vh] md:p-0'>
                   <h1 className='mobile:text-xl md:text-4xl'>
-                    STANDARD ROOM
+                    SUPERIOR ROOM
                   </h1>
                   <p className='px-[5vw] mb-[2vh] mobile:text-sm md:text-lg lg:text-xl'>
-                    Experience comfort and convenience in our Standard rooms, designed for a relaxing stay. These rooms offer modern amenities and a cozy ambiance, providing everything you need for a pleasant retreat.
-                  </p>
+                    Indulge in upgraded comfort with our Superior rooms, featuring enhanced amenities and stylish decor. Enjoy ample space and contemporary design elements, ensuring a luxurious stay experience.                  </p>
                   <div className='flex mobile:flex-col md:flex-row gap-y-[1vh] gap-x-[2vw] items-center  mobile:center md:justify-between px-[5vw]'>
                     <Link
                       href='/accomodation/standard'
@@ -72,7 +71,7 @@ export default function Accomodation({}: Props) {
                   </div>
                 </div>                 
                 <Image
-                  src='/assets/hotelsImg/In2.png'
+                  src='/assets/hotelsImg/In10.png'
                   alt=''
                   width={3840}
                   height={2160}
@@ -83,7 +82,7 @@ export default function Accomodation({}: Props) {
             <li className='bg-gray-800 text-white'>
             <div className='flex flex-row items-center justify-center w-full hover:p-10 transition-all duration-1000'> 
                 <Image
-                  src='/assets/hotelsImg/In2.png'
+                  src='/assets/hotelsImg/in7.png'
                   alt=''
                   width={3840}
                   height={2160}
@@ -91,11 +90,10 @@ export default function Accomodation({}: Props) {
                 />
                 <div className='flex flex-col w-full text-center gap-y-[1vh] mobile:py-[3vh] md:p-0'>
                   <h1 className='mobile:text-xl md:text-4xl'>
-                    STANDARD ROOM
+                    DELUXE ROOM
                   </h1>
                   <p className='px-[5vw] mb-[2vh] mobile:text-sm md:text-lg lg:text-xl'>
-                    Experience comfort and convenience in our Standard rooms, designed for a relaxing stay. These rooms offer modern amenities and a cozy ambiance, providing everything you need for a pleasant retreat.
-                  </p>
+                    Step into luxury with our Deluxe rooms, boasting modern and sophisticated design. These spacious rooms offer panoramic city views of downtown Bangkok, along with a host of deluxe amenities for an unforgettable stay.                  </p>
                   <div className='flex mobile:flex-col md:flex-row gap-y-[1vh] gap-x-[2vw] items-center  mobile:center md:justify-between px-[5vw]'>
                     <Link
                       href='/accomodation/standard'
@@ -117,11 +115,10 @@ export default function Accomodation({}: Props) {
             <div className='flex flex-row items-center justify-center w-full hover:p-10 transition-all duration-1000'> 
                 <div className='flex flex-col w-full text-center gap-y-[1vh] mobile:py-[3vh] md:p-0'>
                   <h1 className='mobile:text-xl md:text-4xl'>
-                    STANDARD ROOM
+                  THREE BED ROOM
                   </h1>
                   <p className='px-[5vw] mb-[2vh] mobile:text-sm md:text-lg lg:text-xl'>
-                    Experience comfort and convenience in our Standard rooms, designed for a relaxing stay. These rooms offer modern amenities and a cozy ambiance, providing everything you need for a pleasant retreat.
-                  </p>
+                    Ideal for families or groups, our Three-Bed rooms provide ample space and comfort for multiple guests. Featuring contemporary furnishings and convenient amenities, these rooms ensure a relaxing and enjoyable stay for all occupants.                  </p>
                   <div className='flex mobile:flex-col md:flex-row gap-y-[1vh] gap-x-[2vw] items-center  mobile:center md:justify-between px-[5vw]'>
                     <Link
                       href='/accomodation/standard'
@@ -138,7 +135,7 @@ export default function Accomodation({}: Props) {
                   </div>
                 </div>              
                 <Image
-                  src='/assets/hotelsImg/In2.png'
+                  src='/assets/hotelsImg/in3.png'
                   alt=''
                   width={3840}
                   height={2160}
