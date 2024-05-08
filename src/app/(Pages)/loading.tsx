5a370ccd0fa480080d1b50d664458c9438877e01
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-05-09 01:30:51 +0700

    Add loading ui

---
+++ src/app/(Pages)/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Loading({}: Props) {
+  return (
+    <main className='w-[100vw] h-[100vh] bg-green-800'>
+        loading...qwer
+    </main>
+  )
+}
